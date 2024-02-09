dba05b040d8b25cc1e7d3ae0cfa2df3030651363
Eva291204 <eva.fricaud@e-artsup.net>
2024-02-09 09:15:22 +0100

    ennemyMain

--- Assets/Scripts/Ennemi/EnnemyActiveState.cs
+++ Assets/Scripts/Ennemi/EnnemyActiveState.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
 public class EnnemyActiveState : IEnnemyState
-{
+{                                             
     public void OnEnter(EnnemyStateMachine ennemyStateMachine)
     {
         Debug.Log("Ennemy ActiveState");
--- Assets/Scripts/Ennemi/MainNameEnnemy.cs
+++ Assets/Scripts/Ennemi/MainNameEnnemy.cs
@@ -8,3 +8,23 @@ public class MainNameEnnemy : MonoBehaviour, IComportement
 
     }
 }
+
+/* 
+ choix du joueur le + proche
+ déplacement vers lui
+
+ si il est assez proche --> attaque
+        choix de l'attaque dans l'actionPool
+            --> attaque à distance
+        ou
+            --> attaque au corps à corps
+
+    si il à encore des points d'actions --> attaque à nouveau
+            --> attaque à distance
+        ou
+            --> attaque au corps à corps
+
+ sinon fin du tour
+
+ */
+
