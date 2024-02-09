d09eef47049007e3b7c24434ffaf3897c464f82e
Yoann | Finiarael <yoann.lemetayer@e-artsup.net>
2024-02-09 09:16:47 +0100

    Accomodation du cursor dans Player

--- Assets/Scripts/Player/PlayerMain.cs
+++ Assets/Scripts/Player/PlayerMain.cs
@@ -4,10 +4,17 @@ public class PlayerMain : Entity
 {
     public EntityData EntityData;
     public PlayerStateMachine StateMachine;
+    public Pointer Pointer;
 
     public override void ResetPoints()
     {
         MPs = EntityData.MPs;
         APs = EntityData.APs;
+    }
+
+    public override void Move(Square destination)
+    {
+        _currentSquare = destination;
+        Pointer.startSquare = _currentSquare;
     }
 }
