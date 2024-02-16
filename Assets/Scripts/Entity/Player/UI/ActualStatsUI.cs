9969297b3ed64c70431d1ff3d78a9499385f0ac4
Eva291204 <eva.fricaud@e-artsup.net>
2024-02-16 22:41:32 +0100

    intégration UI terminer

--- Assets/Scripts/Entity/Player/UI/ActualStatsUI.cs
+++ Assets/Scripts/Entity/Player/UI/ActualStatsUI.cs
@@ -1,10 +1,12 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class ActualStatsUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _textActualMp;
     [SerializeField] private TextMeshProUGUI _textActualAp;
+    [SerializeField] private GameObject _dataPlayers;
     private Entity _entity;
 
     void Start()
@@ -12,8 +14,19 @@ public class ActualStatsUI : MonoBehaviour
         _entity = GetComponent<Entity>();
         _entity.UpadateMpUI += NotifyUdateMP;
         _entity.UpadateApUI += NotifyUdatePA;
-    }
 
+        BattleManager.Instance.AllEntitiesInit += CheckPlayableEntity;
+    }
+    private void CheckPlayableEntity()
+    {
+        for (int i = 0; i < BattleManager.Instance.PlayableEntitiesInBattle.Count; i++)
+        {
+            PlayerMain playableEntity = (PlayerMain)BattleManager.Instance.PlayableEntitiesInBattle[i];
+
+            playableEntity.StateMachine.ActiveState.TurnStarted += ActiveUI;
+            playableEntity.StateMachine.ActiveState.TurnEnded += DesactiveUI;
+        }
+    }
     public void NotifyUdatePA(int newAP)
     {
         _textActualAp.text = "AP : " + newAP.ToString();
@@ -22,5 +35,14 @@ public class ActualStatsUI : MonoBehaviour
     public void NotifyUdateMP(int newMP)
     {
         _textActualMp.text = "MP : " + newMP.ToString();
+    }
+
+    public void ActiveUI()
+    {
+        _dataPlayers.SetActive(true);
+    }
+    public void DesactiveUI()
+    {
+        _dataPlayers.SetActive(false);
     }
 }
